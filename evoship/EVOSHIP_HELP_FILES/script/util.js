8099fc4667501745902d9dda05d283f617ed83af
h035ide <m807007a@mails.cc.ehime-u.ac.jp>
2025-09-16 03:12:30 +0900

    gitignore反映

--- evoship/EVOSHIP_HELP_FILES/script/util.js
+++
@@ -1,8 +0,0 @@
-
-function popupAVI(avifile,x,y,w,h){
-    var wOBJ   = window.createPopup();
-    var popOBJ = wOBJ.document.body;
-	popOBJ.style.border = "solid black 1px";
-	popOBJ.innerHTML = "<img dynsrc='"+avifile+"'/>";
-    wOBJ.show(x,y,w,h,document.body);
-}
--- graphrag_gpt/__init__.py
+++
@@ -1,3 +0,0 @@
-from .ingest0903 import build_databases
-
-__all__ = ["build_databases"]
